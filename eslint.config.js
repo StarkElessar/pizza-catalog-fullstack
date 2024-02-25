540e3ce31f4c340d1761a3c8bcf3000b9bd012d3
stark <serj.elessar@gmail.com>
2024-02-25 17:07:38 +0300

    feat: add eslint.config

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import { resolve } from 'node:path';
+import globals from 'globals';
+import typescriptParser from '@typescript-eslint/parser';
+//! migrate to flat config
+import { FlatCompat } from '@eslint/eslintrc';
+
+const compat = new FlatCompat({
+    baseDirectory: resolve(),
+    resolvePluginsRelativeTo: resolve(),
+});
+//!--------------------------
+
+export default [
+    ...compat.extends('airbnb-base'),
+    {
+        languageOptions: {
+            parser: typescriptParser,
+            globals: {
+                ...globals.node,
+                ...globals.browser,
+            },
+        },
+        rules: {
+            'import/extensions': ['error', { js: 'always' }],
+            'import/no-named-as-default': 'off',
+            'import/no-named-as-default-member': 'off',
+            'import/prefer-default-export': 'off',
+            'no-restricted-syntax': 'off',
+            'prefer-promise-reject-errors': 'off',
+            indent: ['error', 4],
+            'max-len': ['error', { code: 140 }],
+        },
+    },
+];
