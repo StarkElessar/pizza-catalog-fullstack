19d1eb3d46d88449d0e493a88ebc99e3881fcd93
stark <serj.elessar@gmail.com>
2024-02-25 17:07:14 +0300

    fix: update stylelint.config

--- client/stylelint.config.js
+++ client/stylelint.config.js
@@ -1,22 +1,23 @@
 export default {
-	extends: ['@archoleat/stylelint-config-extended-scss'],
+    extends: ['@archoleat/stylelint-config-extended-scss'],
 
-	rules: {
-		'plugin/use-defensive-css': [
-			true,
-			{
-				'accidental-hover': true,
-				'background-repeat': true,
-				'custom-property-fallbacks': false,
-				'flex-wrapping': true,
-				'scroll-chaining': true,
-				'scrollbar-gutter': true,
-				'vendor-prefix-grouping': true,
-			},
-		],
-		'scss/comment-no-loud': null,
-		'scss/at-function-named-arguments': null,
-		'selector-no-qualifying-type': null,
-		'scss/media-feature-value-dollar-variable': null
-	}
-}
+    rules: {
+        'plugin/use-defensive-css': [
+            true,
+            {
+                'accidental-hover': true,
+                'background-repeat': true,
+                'custom-property-fallbacks': false,
+                'flex-wrapping': true,
+                'scroll-chaining': true,
+                'scrollbar-gutter': true,
+                'vendor-prefix-grouping': true,
+            },
+        ],
+        'scss/comment-no-loud': null,
+        'scss/at-function-named-arguments': null,
+        'selector-no-qualifying-type': null,
+        'scss/media-feature-value-dollar-variable': null,
+        'declaration-empty-line-before': 'never',
+    },
+};
