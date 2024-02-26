7a4499e59a8bde8c5c7d4cdf01407156ea58a147
stark <serj.elessar@gmail.com>
2024-02-27 00:52:32 +0300

    fix: update database.service.js; add migrations flow

--- server/services/database.service.js
+++ server/services/database.service.js
@@ -1,4 +1,5 @@
 import { resolve } from 'node:path';
+import { readdir, readFile } from 'node:fs/promises';
 
 export class DatabaseService {
     #db;
@@ -8,14 +9,80 @@ export class DatabaseService {
         this.#logger = logger;
 
         sqlite.verbose();
+	    // Открываем соединение с базой данных
         this.#db = new sqlite.Database(resolve('database.sqlite'));
     }
 
-    connect() {
-        this.#db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, email TEXT)', () => {
-            this.#logger.log('[DatabaseService] Таблица users была успешно создана');
-        });
+    async connect() {
+		await this.#createMigrationsTableIfExists();
+		const migrationsResult = await this.#executeQuery('SELECT * FROM migrations');
+		await this.#applyMigrations(migrationsResult);
     }
+
+	async #applyMigrations(migrationsData) {
+		const migrationDir = resolve('migrations');
+
+		try {
+			const files = await readdir(migrationDir);
+
+			for (const file of files) {
+				const existsRecordIndex = migrationsData.findIndex(({ file_name }) => file_name === file);
+
+				if (existsRecordIndex !== -1) {
+					this.#logger.log(`[DatabaseService] ${file} эта миграция уже была применена`);
+					return;
+				}
+
+				const migrationFile = resolve(migrationDir, file);
+				const migrationScript = await readFile(migrationFile, 'utf8');
+
+				await this.#executeMigration(migrationScript);
+				this.#db.run('INSERT INTO migrations (file_name) VALUES (?)', [file], function(error) {
+					if (error) {
+						console.log({ error })
+					} else {
+						console.log({ lastID: this.lastID, changes: this.changes })
+					}
+				});
+			}
+		} catch (error) {
+			this.#logger.error('[DatabaseService] Ошибка при применении миграций:', error);
+		}
+	}
+
+	async #createMigrationsTableIfExists() {
+		// SQL запрос для проверки существования таблицы и получения записей
+		return this.#executeQuery(`
+	        CREATE TABLE IF NOT EXISTS migrations
+	        (
+	            id INTEGER PRIMARY KEY AUTOINCREMENT,
+	            file_name TEXT NOT NULL,
+	            date DATE DEFAULT CURRENT_TIMESTAMP
+	        );
+		`);
+	}
+
+	#executeQuery(query) {
+		return new Promise((resolve, reject) => {
+			// Выполняем запрос
+			this.#db.serialize(() => {
+				this.#db.all(query, (error, rows) => {
+					if (error) reject(error)
+					resolve(rows);
+				});
+			});
+		});
+	}
+
+	async #executeMigration(migrationScript) {
+		return new Promise((resolve, reject) => {
+			this.#db.exec(migrationScript, (error) => {
+				if (error) reject({ message: 'Произошла ошибка при накатывании миграций', error});
+
+				resolve();
+			});
+		});
+	}
 
     get db() {
         return this.#db;
